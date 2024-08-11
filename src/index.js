9b03386316576dd1a0841e6cabb701ab18e85172
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-08-11 15:26:17 -0400

    Added content to the info div and gave spacing to nav buttons

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import "./style.css";
 const content = document.querySelector("#content");
 
 function component() {
+  const div = document.createElement("div");
+  div.setAttribute("id", "info");
+  content.appendChild(div);
+
   const h1 = document.createElement("h1");
-  h1.textContent = "Restaurant";
+  h1.textContent = "The Velvet Fork";
+  div.appendChild(h1);
 
-
-  return h1;
+  const p = document.createElement("p");
+  p.textContent =
+    "Welcome to The Velvet Fork, where the ambiance is as rich as the flavors. Indulge in a culinary experience like no other, surrounded by warm, ambient lighting and elegant décor that sets the stage for unforgettable moments. Whether you're savoring a romantic dinner or enjoying a night out with friends, our carefully crafted dishes and sophisticated atmosphere promise to leave you wanting more. Discover the perfect blend of luxury and comfort at The Velvet Fork.";
+  div.appendChild(p);
 }
 
 content.appendChild(component());
