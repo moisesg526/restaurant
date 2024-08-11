19c98f527cdc6d82d2632f6e78246eefccd1c0bf
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-08-11 01:29:00 -0400

    Gave color and size to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _ from "lodash";
 import "./style.css";
-import Restautant from "./restaurant.jpg";
 
 const content = document.querySelector("#content");
 
@@ -8,10 +7,7 @@ function component() {
   const h1 = document.createElement("h1");
   h1.textContent = "Restaurant";
 
-  const background = new Image();
-  background.src = Restautant;
 
-  element.appendChild(background);
   return h1;
 }
 
