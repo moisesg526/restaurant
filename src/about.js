9527faf857a18164a5a7b1bfb60248a2454d3a76
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-08-29 23:54:28 -0400

    Added some css to align text and divs to center, also added font sizing

--- src/about.js
+++ src/about.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 export default function about() {
   let aboutDiv = document.createElement("div");
   aboutDiv.setAttribute("div", "aboutDiv");
--- src/home.js
+++ src/home.js
@@ -5,19 +5,20 @@ export default function home() {
   const div = document.createElement("div");
   div.setAttribute("id", "info");
 
+  const text = document.createElement("div");
+  text.setAttribute("id", "text");
+
   const h1 = document.createElement("h1");
   h1.textContent = "The Velvet Fork";
-  div.appendChild(h1);
+  text.appendChild(h1);
 
   const p = document.createElement("p");
   p.textContent =
     "Welcome to The Velvet Fork, where the ambiance is as rich as the flavors. Indulge in a culinary experience like no other, surrounded by warm, ambient lighting and elegant décor that sets the stage for unforgettable moments. Whether you're savoring a romantic dinner or enjoying a night out with friends, our carefully crafted dishes and sophisticated atmosphere promise to leave you wanting more. Discover the perfect blend of luxury and comfort at The Velvet Fork.";
-  div.appendChild(p);
-  content.appendChild(div);
+  text.appendChild(p);
 
   const imageDiv = document.createElement("div");
   imageDiv.setAttribute("id", "imgDiv");
-  content.appendChild(imageDiv);
 
   const dish = new Image();
   const darkMeal = new Image();
@@ -25,6 +26,11 @@ export default function home() {
   darkMeal.src = DarkMeal;
   imageDiv.appendChild(darkMeal);
   imageDiv.appendChild(dish);
+
+
+  div.appendChild(text);
+  div.appendChild(imageDiv);
+  content.appendChild(div);
 
   return div;
 }
